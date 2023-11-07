f9303c58fa0743ee51325b050fa377fc5dbe405e
Jourloy <jourloy@yandex.ru>
2023-11-07 04:56:00 +0300

    🔨 Add mantine and tailwind support

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,11 +1,12 @@
+import '@mantine/core/styles.css';
 import "./index.css";
 import App from "./App.tsx";
-import DefaultContainer from "./containers/index.tsx";
 import ReactDOM from "react-dom/client";
+import MantineContainer from './containers/mantine/mantine.tsx';
 
 const container = document.getElementById(`root`);
 ReactDOM.createRoot(container!).render(
-	<DefaultContainer>
+	<MantineContainer>
 		<App />
-	</DefaultContainer>
+	</MantineContainer>
 );
