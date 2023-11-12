4c8aa7155e0b35ba75306c524e0c4e636589ad88
Jourloy <jourloy@yandex.ru>
2023-11-12 04:10:06 +0300

    🔨 Add containers

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,12 +1,15 @@
-import '@mantine/core/styles.css';
+import "@mantine/core/styles.css";
 import "./index.css";
 import App from "./App.tsx";
 import ReactDOM from "react-dom/client";
-import MantineContainer from './containers/mantine/mantine.tsx';
+import MantineContainer from "./containers/mantine/mantine.tsx";
+import ReactQueryContainer from "./containers/reactQuery/index.tsx";
 
 const container = document.getElementById(`root`);
 ReactDOM.createRoot(container!).render(
-	<MantineContainer>
-		<App />
-	</MantineContainer>
+	<ReactQueryContainer>
+		<MantineContainer>
+			<App />
+		</MantineContainer>
+	</ReactQueryContainer>
 );
