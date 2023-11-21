a6e8f0a828495c005bdd602da248ba9ea3f76aaa
Jourloy <jourloy@yandex.ru>
2023-11-21 20:20:50 +0300

    🔨 Add dayjs

--- apps/frontend/src/containers/mantine/mantine.tsx
+++ apps/frontend/src/containers/mantine/mantine.tsx
@@ -1,13 +1,18 @@
 import "@mantine/notifications/styles.css";
+import "@mantine/dates/styles.css";
+import "dayjs/locale/ru";
 import {MantineProvider} from "@mantine/core";
 import {PropsWithChildren} from "react";
-import { Notifications } from "@mantine/notifications";
+import {Notifications} from "@mantine/notifications";
+import {DatesProvider} from "@mantine/dates";
 
 export default function MantineContainer(props: PropsWithChildren) {
 	return (
 		<MantineProvider>
-			<Notifications />
-			{props.children}
+			<DatesProvider settings={{locale: `ru`, firstDayOfWeek: 1}}>
+				<Notifications />
+				{props.children}
+			</DatesProvider>
 		</MantineProvider>
 	);
 }
