ed2594107e4792fe6835f4cb748b32a01400da15
Jourloy <jourloy@yandex.ru>
2023-11-22 06:54:16 +0300

    🔨 Add containers

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -4,12 +4,18 @@ import App from "./App.tsx";
 import ReactDOM from "react-dom/client";
 import MantineContainer from "./containers/mantine/mantine.tsx";
 import ReactQueryContainer from "./containers/reactQuery/index.tsx";
+import DefaultContainer from "./containers/default/index.tsx";
+import {Providers} from "./store/provider.tsx";
 
 const container = document.getElementById(`root`);
 ReactDOM.createRoot(container!).render(
-	<ReactQueryContainer>
+	<Providers>
 		<MantineContainer>
-			<App />
+			<ReactQueryContainer>
+				<DefaultContainer>
+					<App />
+				</DefaultContainer>
+			</ReactQueryContainer>
 		</MantineContainer>
-	</ReactQueryContainer>
+	</Providers>
 );
